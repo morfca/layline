7dadc892c3fa2bc36dde4780b84bb8b8e8e975ca
Anthony Roberts <acrobert@gmail.com>
2020-09-30 11:52:35 -0700

    update for clap API changes

--- src/main.rs
+++ src/main.rs
@@ -7,47 +7,47 @@ mod server;
 
 fn main() {
 	let matches = App::new("layline")
-		.version("0.1")
+		.version("0.2")
 		.author("Anthony Roberts <acrobert@gmail.com>")
 		.about("tunnel connections via HTTP")
-		.arg(Arg::with_name("SESSION_TIMEOUT")
+		.arg(Arg::new("SESSION_TIMEOUT")
 			.about("inactivity timeout for sessions in seconds, default=900")
 			.long("session-timeout")
 			.takes_value(true))
-		.arg(Arg::with_name("MAX_SESSIONS")
+		.arg(Arg::new("MAX_SESSIONS")
 			.about("maximum simultaneous sessions allowed, default=100")
 			.long("max-sessions")
 			.takes_value(true))
-		.arg(Arg::with_name("ALLOW_PLAINTEXT")
+		.arg(Arg::new("ALLOW_PLAINTEXT")
 			.about("allow outbound connections to use plaintext in client modes")
 			.long("allow-plaintext"))
 		.subcommand(App::new("server")
 			.about("run as a server")
-			.arg(Arg::with_name("LISTEN_PORT")
+			.arg(Arg::new("LISTEN_PORT")
 				.about("ip:port to listen on for tunneled connections")
 				.required(true)
 				.index(1))
-			.arg(Arg::with_name("DEST_PORT")
+			.arg(Arg::new("DEST_PORT")
 				.about("ip:port to connect to for tunneled connections")
 				.required(true)
 				.index(2))
-			.arg(Arg::with_name("LOG_DEST")
+			.arg(Arg::new("LOG_DEST")
 				.long("log-dest")
 				.about("a directory to log to, if unspecified stderr will be used")
 				.takes_value(true)))
 		.subcommand(App::new("client")
 			.about("run as a client, with a persistent process to forward connections")
-			.arg(Arg::with_name("LISTEN_PORT")
+			.arg(Arg::new("LISTEN_PORT")
 				.about("ip:port to listen on for tunneled connections")
 				.required(true)
 				.index(1))
-			.arg(Arg::with_name("DEST_URL")
+			.arg(Arg::new("DEST_URL")
 				.about("URL prefix for the HTTP path that exposes the layline API")
 				.required(true)
 				.index(2)))
 		.subcommand(App::new("proxyclient")
 			.about("run as a client, automatically establishing a session and using stdin/stdout")
-			.arg(Arg::with_name("DEST_URL")
+			.arg(Arg::new("DEST_URL")
 				.about("URL prefix for the HTTP path that exposes the layline API")
 				.required(true)
 				.index(1)))
@@ -63,18 +63,18 @@ fn main() {
 	let allow_plaintext = matches.is_present("ALLOW_PLAINTEXT");
 	let opts = (max_sessions, timeout_sessions, allow_plaintext);
 	match matches.subcommand() {
-		("proxyclient", Some(matches)) => {
+		Some(("proxyclient", matches)) => {
 			let dest_port = matches.value_of("DEST_URL").unwrap();
 			let ret = client::proxy_run(dest_port, opts);
 			std::process::exit(ret);
 		}
-		("client", Some(matches)) => {
+		Some(("client", matches)) => {
 			let listen_port = matches.value_of("LISTEN_PORT").unwrap();
 			let dest_port = matches.value_of("DEST_URL").unwrap();
 			let ret = client::run(listen_port, dest_port, opts);
 			std::process::exit(ret);
 		}
-		("server", Some(matches)) => {
+		Some(("server", matches)) => {
 			let listen_port = matches.value_of("LISTEN_PORT").unwrap();
 			let dest_port = matches.value_of("DEST_PORT").unwrap();
 			let log_dest = match matches.value_of("LOG_DEST") {
